2a76885472f170739a7d32866edb1578f02e948e
hadeel480 <77208260+hadeel480@users.noreply.github.com>
2021-07-14 16:20:55 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React,{component} from 'react';
+import Body from './components/Body';
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+
+
+function App() {
+ return(
+  <div>
+    <Header/>
+    <Body/>
+    <Footer/>
+</div>
+
+  
+  
+  
+  );
+}
+
+export default App;
